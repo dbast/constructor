ae79a4bee8407ec282f570853e02de8c61e6a5c5
Daniel Bast <2790401+dbast@users.noreply.github.com>
2023-01-23 14:45:51 +0100

    Cache header.sh and use loops to speedup unit tests

--- tests/test_header.py
+++ tests/test_header.py
@@ -1,3 +1,4 @@
+import itertools
 from functools import lru_cache
 from pathlib import Path
 import subprocess
@@ -14,6 +15,11 @@ from constructor.utils import preprocess
 @lru_cache()
 def available_command(cmd):
     return which(cmd) is not None
+
+
+@lru_cache()
+def fast_read_header_template():
+    return read_header_template()
 
 
 def run_shellcheck(script):
@@ -33,48 +39,64 @@ def run_shellcheck(script):
     return findings, p.returncode
 
 
-@pytest.mark.parametrize('osx', [False, True])
-@pytest.mark.parametrize('direct_execute_post_install', [False, True])
-@pytest.mark.parametrize('direct_execute_pre_install', [False, True])
-@pytest.mark.parametrize('batch_mode', [False, True])
-@pytest.mark.parametrize('keep_pkgs', [False, True])
-@pytest.mark.parametrize('has_conda', [False, True])
-@pytest.mark.parametrize('has_license', [False, True])
-@pytest.mark.parametrize('initialize_conda', [False, True])
-@pytest.mark.parametrize('initialize_by_default', [False, True])
-@pytest.mark.parametrize('has_post_install', [False, True])
-@pytest.mark.parametrize('has_pre_install', [False, True])
-@pytest.mark.parametrize('check_path_spaces', [False, True])
-@pytest.mark.parametrize('arch', ['x86', 'x86_64', ' ppc64le', 's390x', 'aarch64'])
-def test_linux_template_processing(
-        osx, arch, has_pre_install, has_post_install, initialize_conda,
-        initialize_by_default, has_license, has_conda, keep_pkgs, batch_mode,
-        direct_execute_pre_install, direct_execute_post_install, check_path_spaces):
-    template = read_header_template()
-    processed = preprocess(template, {
-       'has_license': has_license,
-       'osx': osx,
-       'batch_mode': batch_mode,
-       'keep_pkgs': keep_pkgs,
-       'has_conda': has_conda,
-       'x86': arch == 'x86',
-       'x86_64': arch == 'x86_64',
-       'ppc64le': arch == 'ppc64le',
-       's390x': arch == 's390x',
-       'aarch64': arch == 'aarch64',
-       'linux': not osx,
-       'has_pre_install': has_pre_install,
-       'direct_execute_pre_install': direct_execute_pre_install,
-       'has_post_install': has_post_install,
-       'direct_execute_post_install': direct_execute_post_install,
-       'initialize_conda': initialize_conda,
-       'initialize_by_default': initialize_by_default,
-       'check_path_spaces': check_path_spaces,
-
-    })
-    assert '#if' not in processed
-    assert '#else' not in processed
-    assert '#endif' not in processed
+def test_linux_template_processing():
+    ft = [False, True]
+    for (
+        osx,
+        direct_execute_post_install,
+        direct_execute_pre_install,
+        batch_mode,
+        keep_pkgs,
+        has_conda,
+        has_license,
+        initialize_conda,
+        initialize_by_default,
+        has_post_install,
+        has_pre_install,
+        check_path_spaces,
+        arch,
+    ) in itertools.product(
+        ft,
+        ft,
+        ft,
+        ft,
+        ft,
+        ft,
+        ft,
+        ft,
+        ft,
+        ft,
+        ft,
+        ft,
+        ["x86", "x86_64", " ppc64le", "s390x", "aarch64"],
+    ):
+        template = fast_read_header_template()
+        processed = preprocess(
+            template,
+            {
+                "has_license": has_license,
+                "osx": osx,
+                "batch_mode": batch_mode,
+                "keep_pkgs": keep_pkgs,
+                "has_conda": has_conda,
+                "x86": arch == "x86",
+                "x86_64": arch == "x86_64",
+                "ppc64le": arch == "ppc64le",
+                "s390x": arch == "s390x",
+                "aarch64": arch == "aarch64",
+                "linux": not osx,
+                "has_pre_install": has_pre_install,
+                "direct_execute_pre_install": direct_execute_pre_install,
+                "has_post_install": has_post_install,
+                "direct_execute_post_install": direct_execute_post_install,
+                "initialize_conda": initialize_conda,
+                "initialize_by_default": initialize_by_default,
+                "check_path_spaces": check_path_spaces,
+            },
+        )
+        assert "#if" not in processed
+        assert "#else" not in processed
+        assert "#endif" not in processed
 
 
 @pytest.mark.parametrize("arch", ["x86_64", "arm64"])
@@ -132,7 +154,7 @@ def test_template_shellcheck(
     direct_execute_post_install,
     check_path_spaces,
 ):
-    template = read_header_template()
+    template = fast_read_header_template()
     processed = preprocess(
         template,
         {
